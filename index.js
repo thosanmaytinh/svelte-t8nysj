a5a391ee53434b989a2b479a2957fe6a795131b9
thosanmaytinh <thosanmaytinh@yahoo.com>
2019-04-16 17:39:13 +0700

    agile

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ const AppComponent = new App({
   target: document.querySelector('#root'),
   data: { name: 'svelte' }
 });
+
+const Aigle = require('aigle');
+
+let api = [
+  {
+    name: 'openload.co',
+    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+    maxFileSize: 10
+  },
+  {
+    name: 'test.test',
+    urlUpload: 'https://api.test.co/',
+    maxFileSize: 20
+  }
+]
+var ts  = {
+  length: 10
+}
+Aigle.resolve(api)
+  .pickBySeries(host => {
+    return host.maxFileSize >= ts.length
+  })
+  .then(array => {
+    console.log(array)
+  });
