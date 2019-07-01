332c49f4cdf6f94ee5b7f978ceb9cf1082d791a5
thosanmaytinh <thosanmaytinh@yahoo.com>
2019-07-01 21:22:21 +0700

    1 file modified

--- index.js
+++ index.js
@@ -8,65 +8,75 @@ const AppComponent = new App({
 
 const Aigle = require('aigle');
 
-let api = [
-  {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  },
-  {
-    name: 'iocloud.info',
-    urlUpload: 'https://api.test.co/',
-    maxFileSize: 20
-  },
-  {
-    name: 'tinhte.vn/index.php',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }, {
-    name: 'tinhte.vn',
-    urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
-    maxFileSize: 10
-  }
-]
-var ts = {
-  length: 10
-}
-Aigle.resolve(api)
-  .mapSeries(async host => {
-    return $.get(host.name).then(async r => {
-      await Aigle.delay(3000)
-      console.log(new Date(), host.name)
-      return r
-    })
-    // console.log(host.name)
-    // return a
-  })
+// let api = [
+//   {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   },
+//   {
+//     name: 'iocloud.info',
+//     urlUpload: 'https://api.test.co/',
+//     maxFileSize: 20
+//   },
+//   {
+//     name: 'tinhte.vn/index.php',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }, {
+//     name: 'tinhte.vn',
+//     urlUpload: 'https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX',
+//     maxFileSize: 10
+//   }
+// ]
+// var ts = {
+//   length: 10
+// }
+// Aigle.resolve(api)
+//   .mapSeries(async host => {
+//     return $.get(host.name).then(async r => {
+//       await Aigle.delay(3000)
+//       console.log(new Date(), host.name)
+//       return r
+//     })
+//     // console.log(host.name)
+//     // return a
+//   })
+let called = 0;
+Aigle.retry(3, () => {
+  return new Aigle((resolve, reject) => {
+    setTimeout(() => reject(++called), 10);
+  });
+})
+.catch(error => {
+  console.log(error); // 3
+  console.log(called); // 3
+});
