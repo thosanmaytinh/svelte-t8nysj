b2e7c75f26bdae880cd1290886819a0cde4bfa42
thosanmaytinh <thosanmaytinh@yahoo.com>
2020-03-19 22:34:33 +0700

    1 file modified

--- index.js
+++ index.js
@@ -9,128 +9,154 @@ const AppComponent = new App({
 const Aigle = require("aigle");
 Aigle.mixin(require("lodash"));
 
-let api = [
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "iocloud.info",
-    urlUpload: "https://api.test.co/",
-    maxFileSize: 20
-  },
-  {
-    name: "tinhte.vn/index.php",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  },
-  {
-    name: "tinhte.vn",
-    urlUpload:
-      "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
-    maxFileSize: 10
-  }
-];
-var ts = {
-  length: 10
-};
-
-// Aigle.resolve(api).mapSeries(async host => {
-//   return $.get(host.name).then(async r => {
-//     console.log(new Date(), host.name);
-//     return r;
-//   });
-// });
-
-// let called = 0;
-// Aigle.retry(3, () => {
-//   return new Aigle((resolve, reject) => {
-//     setTimeout(() => reject(++called), 10);
-//   });
-// }).catch(error => {
-//   console.log(error); // 3
-//   console.log(called); // 3
-// });
-
-// Aigle.resolve([{thinh:1}, {thinh:2}, {thinh:2}])
-// .filterSeries(item => {
-//   if (item.thinh === 1) {
-//     return false;
+// let api = [
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "iocloud.info",
+//     urlUpload: "https://api.test.co/",
+//     maxFileSize: 20
+//   },
+//   {
+//     name: "tinhte.vn/index.php",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
+//   },
+//   {
+//     name: "tinhte.vn",
+//     urlUpload:
+//       "https://api.openload.co/1/file/ul?login=7cf46c19eaac9aee&key=iEwhiOuX",
+//     maxFileSize: 10
 //   }
-//   return item
-// })
-// .then(console.log)
+// ];
+// var ts = {
+//   length: 10
+// };
 
-// Example
-const a = [{ name: "aaaa" }, { name: "aaaaaaaaaa2" }];
-const b = [{ name: "bbbbb" }, { name: "bbbbb22222" }];
-Aigle
-  .chain([1, 2], [10, 20], [100, 200])
-  .zipWith((a, b, c) => {
-  console.log(a, b, c);
-});
+// // Aigle.resolve(api).mapSeries(async host => {
+// //   return $.get(host.name).then(async r => {
+// //     console.log(new Date(), host.name);
+// //     return r;
+// //   });
+// // });
+
+// // let called = 0;
+// // Aigle.retry(3, () => {
+// //   return new Aigle((resolve, reject) => {
+// //     setTimeout(() => reject(++called), 10);
+// //   });
+// // }).catch(error => {
+// //   console.log(error); // 3
+// //   console.log(called); // 3
+// // });
+
+// // Aigle.resolve([{thinh:1}, {thinh:2}, {thinh:2}])
+// // .filterSeries(item => {
+// //   if (item.thinh === 1) {
+// //     return false;
+// //   }
+// //   return item
+// // })
+// // .then(console.log)
+
+// // Example
+// const a = [{ name: "aaaa" }, { name: "aaaaaaaaaa2" }];
+// const b = [{ name: "bbbbb" }, { name: "bbbbb22222" }];
+// Aigle
+//   .chain([1, 2], [10, 20], [100, 200])
+//   .zipWith((a, b, c) => {
+//   console.log(a, b, c);
+// });
 
 
-//0000
-const fromTimestamp = 1579520740857 // 2020-01-20T11:45:40.857Z
-const toTimestamp = 1582112812032   // 2020-02-19T11:46:52.032Z
+// //0000
+// const fromTimestamp = 1579520740857 // 2020-01-20T11:45:40.857Z
+// const toTimestamp = 1582112812032   // 2020-02-19T11:46:52.032Z
 
 
-for(
-  let dateFromInput = new Date('2020-03-02');
-  dateFromInput <= new Date('2020-03-09');
-  dateFromInput.setUTCDate(dateFromInput.getUTCDate() + 2)
-) {
-  let _date = new Date(dateFromInput);
-  _date.setUTCDate(_date.getUTCDate() + 1)
-  _date.setUTCHours(23, 59, 59)
-  const fromDateTimeStamp = Math.round(dateFromInput.getTime() / 1000)
-  const toDateTimeStamp = Math.round(_date.getTime() / 1000)
+// for(
+//   let dateFromInput = new Date('2020-03-02');
+//   dateFromInput <= new Date('2020-03-09');
+//   dateFromInput.setUTCDate(dateFromInput.getUTCDate() + 2)
+// ) {
+//   let _date = new Date(dateFromInput);
+//   _date.setUTCDate(_date.getUTCDate() + 1)
+//   _date.setUTCHours(23, 59, 59)
+//   const fromDateTimeStamp = Math.round(dateFromInput.getTime() / 1000)
+//   const toDateTimeStamp = Math.round(_date.getTime() / 1000)
 
-  console.log(fromDateTimeStamp, toDateTimeStamp, '|||')
-}
+//   console.log(fromDateTimeStamp, toDateTimeStamp, '|||')
+// }
+
+
+// function computeGridTrading(bottom, top, row, perVolume) {
+//         const distance = (bottom - top) / (row - 1)
+//         const result = []
+//         for (let index = bottom; index <= top; index += distance) {
+//             result.push(index)
+//         }
+        
+
+//         return result
+//   }
+
+// console.log(computeGridTrading(3.9451, 4.4525, 5))
+console.log('hth')
+const bottom = 3.9451
+const top = 4.4525
+const row = 5
+ const distance = (bottom - top) / (row - 1)
+        const result = []
+        for (let index = bottom; index <= top; index += distance) {
+            result.push(index)
+        }
+        
+
+        return result
